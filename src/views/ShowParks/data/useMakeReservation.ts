8fe5bcc91005bab2acf5eb54b41c45032bcef73d
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2025-01-19 20:07:58 +0300

    fix wrong api route

--- src/views/ShowParks/data/useMakeReservation.ts
+++ src/views/ShowParks/data/useMakeReservation.ts
@@ -3,7 +3,7 @@ import { api } from "src/utils";
 
 export function useMakeReservation() {
   async function makeReservation(data: RentForm & { parkId: number }) {
-    return await api.post("/park/reserve", data);
+    return await api.post("/booking/create", data);
   }
 
   return { makeReservation };
